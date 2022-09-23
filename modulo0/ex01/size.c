f1f58afdecebc72aa232ca577fb8d83003538721
Joao <1210957@isep.ipp.pt>
2022-09-23 10:58:39 +0100

    exercicio 01 sobre tamanho de variaveis

---
+++ modulo0/ex01/size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("\nSize of char: %lu",sizeof(char));
+    printf("\nSize of int: %lu",sizeof(int));
+    printf("\nSize of unsigned int: %lu",sizeof(unsigned int));
+    printf("\nSize of long: %lu",sizeof(long));
+    printf("\nSize of short: %lu",sizeof(short));
+    printf("\nSize of long long: %lu",sizeof(long long));
+    printf("\nSize of float: %lu",sizeof(float));
+    printf("\nSize of double: %lu\n",sizeof(double));
+}
