036e94260c0a39295ae03f1beab837891b67db9e
José Diogo Nápoles de Sousa Rente <1211155@isep.ipp.pt>
2022-10-07 02:51:47 +0100

    ex10 bug corrections

--- modulo1/ex10/function.c
+++ modulo1/ex10/function.c
@@ -2,13 +2,13 @@ int odd_sum(int *p)
 {
     int sum, i, arraySize;
     sum = 0;
-    arraySize = (int)p[0]+1;
-
-    for(i=1; i<arraySize; i++)
+    arraySize = (int)*p+1;
+    
+    for(i=1; i < arraySize; i++)
     {
-        if(p[i]%2==1 || p[i]%2==-1)
-            sum=sum+p[i];
+        p++;
+        if((*p)%2==1 || (*p)%2==-1)
+            sum=sum +(*p);
     }
-
     return sum;
 }
