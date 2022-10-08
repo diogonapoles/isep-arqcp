520d397a5456a9af849f098dd6476620d076daae
José Diogo Nápoles de Sousa Rente <1211155@isep.ipp.pt>
2022-10-08 20:44:42 +0100

    visual changes (ex16)

--- modulo1/ex16/main.c
+++ modulo1/ex16/main.c
@@ -9,8 +9,10 @@ char frase2[]= "Sera que existe algo";
 
 char *ap1 = where_exists(frase1,frase2);
 
+printf("String completa: %s\n",frase2);
+printf("String para procurar: %s\n",frase1);
 
-printf("\n%s\n",ap1);
+printf("Inicio da primeira ocorrencia de (%s), em (%s): %s\n",frase1, frase2, ap1);
 
 
 return 0;
