bcfd61975aef2753572bf0da65985282a1288f0d
José Diogo Nápoles de Sousa Rente <1211155@isep.ipp.pt>
2022-10-09 01:59:07 +0100

    ex20

---
+++ modulo1/ex20/function.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+#include "function.h"
+
+#define COMPARAR(C1, C2)    (tolower((C1)) != tolower((C2)))
+
+void find_all_words(char* str, char* word, char** addrs){
+	char *a, *b;
+
+    b = word;
+    if (*b == 0) 
+	{
+		return;
+    }
+    
+    for ( ; *str != '\0'; str ++) {
+		if (*str != *b) {//procurar um carater em comum, quando for encontrado vamos comparar o resto
+			continue; 
+		}
+
+		a = str;
+		while (1) {//loop "infinito"
+			if (*b == '\0') {//chegar ao fim
+				*addrs = a;
+				addrs++;
+			}
+			if (COMPARAR(*a++, *b++)){//comparar em lower case
+				break; // exit the loop
+				
+			}
+		}
+		b = word;
+    } 
+}
+  
---
+++ modulo1/ex20/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include "function.h"
+
+int main(){
+	char* word = "xx";
+	char str[] = "rxxX oxxad xxdafxx xx";
+
+	printf("String completa: %s\n",str);
+	printf("String para procurar: %s\n",word);
+
+	unsigned int length = strlen(str);
+
+	char* addrs[length];
+	
+	find_all_words(str,word,addrs);
+
+	printf("\nEndereços encontrados:\n");
+
+	int j;
+	while (addrs[j] != NULL)
+	{
+		printf("%p\n", addrs[j]);
+		j++;
+	}
+
+	return 0;
+}
